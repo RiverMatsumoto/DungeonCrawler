07f87c740cf4f6fe89760fd0ac8b2562cae9408b
RiverMatsumoto <matsumotorjames@gmail.com>
2021-11-10 01:32:53 -1000

    Working on minimap.

---
+++ Scripts/MinimapPlayer.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MinimapPlayer : MonoBehaviour
+{
+    public PlayerMovement playerMovement;
+
+    private void OnEnable()
+    {
+        
+    }
+}
